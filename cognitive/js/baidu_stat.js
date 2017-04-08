9d04ef63a6224bb594c754406306a5843e8735f2
lostsquirrel <songli1988@gmail.com>
2017-04-08 17:15:35 +0800

    add baidu statistics

---
+++ cognitive/js/baidu_stat.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?6985ff14d22974f15eabdad96bc2bb57";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
